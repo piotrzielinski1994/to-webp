afc37a8c335946d1ad680ef1ffd926795296d11f
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2022-06-12 12:31:31 +0200

    Clean index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer({ dest: './uploads/' }).single('file'));
 
 app.post('/', async (req, res) => {
   const quality = Number(req.body?.quality) || 75;
-  // res.send({ body: req.body, quality });
-  // return;
   const fileName = req.file.path;
+
   try {
     const image = await sharp(fileName).webp({ quality }).toBuffer();
     res.end(image, 'binary');
